602bc5a118192cd980e3786bd86d91f9a8d8bab8
Ornella Sathoud <osathoud@gmail.com>
2019-05-12 19:34:19 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -16,5 +16,6 @@ plot(Sub_metering_1~datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~datetime,col='Red')
   lines(Sub_metering_3~datetime,col='Blue')})
+legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 dev.copy(png,"plot3.png", width=480, height=480)
 dev.off()
